087260ef4d866b632f244e53f3c5f0de723e557d
rifyalizzi <44332244+rifyalizzi@users.noreply.github.com>
2018-10-21 13:32:02 +0700

    Update Tugas.py
    tes

---
+++ Tugas.py
@@ -0,0 +1,27 @@
+
+weight = 58
+height = 169
+
+#Formula Perhitungan BMI
+BMI = weight / ((height/100) ** 2)
+
+print("Kategori BMI Anda: ")
+
+if BMI < 15:
+    print("Very severely underweight")
+elif BMI >= 15 and BMI < 16:
+    print("Severely underweight")
+elif BMI >= 16 and BMI < 18.5:
+    print("Underweight")
+elif BMI >= 18.5 and BMI < 25:
+    print("Normal (healthy weight)")
+elif BMI >= 25 and BMI < 30:
+    print("Overweight")
+elif BMI >= 30 and BMI < 35:
+    print("Moderately obese")
+elif BMI >= 35 and BMI < 40:
+    print("Severely obese")
+else:
+    print("Very severely obese")
+
+print("Done")
